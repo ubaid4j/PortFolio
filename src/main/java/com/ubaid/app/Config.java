5c4897271255393ce6003730ef3adc9743c68e73
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-07-22 16:08:15 +0500

    ready for deploy

--- src/main/java/com/ubaid/app/Config.java
+++ src/main/java/com/ubaid/app/Config.java
@@ -2,11 +2,13 @@ package com.ubaid.app;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan("com.ubaid.app")
+@EnableWebMvc
 public class Config implements WebMvcConfigurer
 {
 
